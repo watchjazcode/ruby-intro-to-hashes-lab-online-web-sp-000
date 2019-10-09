e59066e772db2744dd9de416372e63028d40b9b5
Jazmin M <jazminmerckel@gmail.com>
2019-10-09 02:35:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -60,9 +60,11 @@ def monopoly_with_fourth_tier
 	        "mortage_value" => "100"
 	      },
 	      :pennsylvania_railroad => {
-	        ""
+	        "mortage_value" => "100"
 	      },
-	      :b_and_o_railroad => {},
+	      :b_and_o_railroad => {
+	        
+	      },
 	      :shortline => {}
 	    },
 	    :rent_in_dollars => {
